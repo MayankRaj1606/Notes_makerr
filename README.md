d9bfb873beb6f73054a21d81bf60a81d24a1c785
Mayank-cspl <mayankraj.cspl@gmail.com>
2025-02-05 01:14:41 +0530

    Second commit

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # Notes_maker
+
+
+NotesMaker is a structured note-taking web application built with React.js/Next.js, MongoDB, and JWT authentication. It supports text, URL, and audio input, allowing users to create, transcribe, and manage notes efficiently.
+
+Features
+
+User authentication with JWT (Sign-up & Login)
+
+Create and manage structured notes
+
+Support for text and URL input
+
+Audio recording (max 1 min) with automatic transcription
+
+Notes stored in MongoDB
+
+Notes sorting from oldest to newest
+
+Responsive and user-friendly UI
+
+Tech Stack
+
+Frontend: React.js/Next.js, Tailwind CSS
+
+Backend: Node.js, Express.js
+
+Database: MongoDB
+
+Authentication: JWT
+
+Audio Transcription: Web Speech API
+
+Installation
+
+Prerequisites
+
+Ensure you have the following installed:
+
+Node.js
+
+MongoDB
+
+Contributing
+
+Contributions are welcome! Feel free to open issues and pull requests.
---
+++ routes/productRoutes.js
@@ -0,0 +1,52 @@
+import express from 'express'
+import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js'
+import {
+    createProductController, deleteProductController, getAllUsersController, getProductController,
+    getSingleProductController, productCountController, productFiltersController, productListController, productPhotoController,
+    updateNameProductController,
+    updateProductController,
+    updateuserProductController
+} from '../controllers/productController.js'
+import formidable from 'express-formidable'
+
+const router = express.Router()
+
+//routes
+router.post('/create-product',  formidable(), createProductController)
+
+router.put('/update-product/:pid', requireSignIn,formidable(),  updateProductController) 
+
+router.put('/update-userProduct/:pid', requireSignIn, formidable(), updateuserProductController )
+
+router.put('/update-name/:pid', requireSignIn,   updateNameProductController)
+
+router.get('/get-product', getProductController)
+
+
+
+//fetching single product
+router.get("/get-product/:slug", getSingleProductController)
+
+
+router.get('/product-photo/:pid', productPhotoController)
+
+
+
+//delete product
+router.delete("/delete-product/:pid", requireSignIn,  deleteProductController);
+
+
+//filter product
+router.get("/product-filters" , productFiltersController)
+
+//product count
+router.get("/product-count", productCountController);
+
+//product per page
+router.get("/product-list/:page", productListController);
+
+
+//users data
+router.get("/get-users", getAllUsersController);
+
+export default router
